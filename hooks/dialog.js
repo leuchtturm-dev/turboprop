a6ee9d28c0642df9c307cd05797de3b07b31791e
Christoph Schmatzler <christoph@medium.place>
2024-06-12 12:14:55 +0200

    finish dialog

--- hooks/dialog.js
+++ hooks/dialog.js
@@ -3,7 +3,18 @@ import { normalizeProps, renderPart } from "./util";
 
 export const Dialog = {
   mounted() {
-    this.context = { id: this.el.id };
+    this.context = {
+      id: this.el.id,
+      role: this.el.dataset.role,
+      preventScroll: this.el.dataset.preventScroll === "true" || this.el.dataset.preventScroll === "",
+      closeOnInteractOutside: this.el.dataset.closeOnInteractOutside === "true" || this.el.dataset.closeOnInteractOutside === "",
+      closeOnEscape: this.el.dataset.closeOnEscape === "true" || this.el.dataset.closeOnEscape === "",
+      onOpenChange(details) {
+        if (self.el.dataset.onOpenChange) {
+          self.pushEvent(self.el.dataset.onOpenChange, details);
+        }
+      },
+    };
 
     this.service = dialog.machine(this.context);
     this.api = dialog.connect(this.service.state, this.service.send, normalizeProps);
@@ -26,7 +37,7 @@ export const Dialog = {
   },
 
   render() {
-    parts = ["trigger", "backdrop", "positioner", "content", "title", "description", "close-trigger"];
-    parts.forEach((part) => renderPart(this.el, part, this.api));
+    const parts = ["trigger", "backdrop", "positioner", "content", "title", "description", "close-trigger"];
+    for (const part of parts) renderPart(this.el, part, this.api);
   },
 };
